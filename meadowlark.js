8bcf3c0ac99289866594185c4905e4d5f5592fbb
Sasha Good aka icobra <eiroglif@mail.ru>
2017-11-13 16:45:41 +0300

    Fix code

--- meadowlark.js
+++ meadowlark.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
+
 var app = express();
 // Установка механизма представления hadlebars
-var handlebars = require('express-handlebars')
-   .create({ defaultLayout:'main'});
-var fortune = require('./lib/fortune.js')
-
+var handlebars = require('express-handlebars').create({
+        defaultLayout:'main',       
+      helpers: {      
+         section: function(name, options){       
+             if(!this._sections) this._sections = {};        
+             this._sections[name] = options.fn(this);        
+             return null;        
+         }       
+     }       
+});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -13,11 +21,30 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+// set 'showTests' для включения тестов используйте test=1
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
 app.get('/', function(req, res){
     res.render ('home');
 });
 app.get('/about', function(req, res){
-    res.render('about', { fortune: fortune.getFortune() });
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){      
+    res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
 });
 // Обобщенный обработчик 404 (промежуточное ПО)
 app.use(function(req, res, next){
